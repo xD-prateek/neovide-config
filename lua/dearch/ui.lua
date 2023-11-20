7934704306425b5144b9bba55c5b21c169800a8a
Prateek <pranav.chiddarwar@gitlab.com>
2023-11-20 20:13:51 +0530

    updated status line ui

--- lua/dearch/ui.lua
+++ lua/dearch/ui.lua
@@ -18,7 +18,6 @@ end
 
 -- set up lualine
 local lualine_status_ok, lualine = pcall(require, "lualine")
-
 if lualine_status_ok then
 	local colors = {
 		blue   = '#80a0ff',
@@ -28,41 +27,82 @@ if lualine_status_ok then
 		red    = '#ff5189',
 		violet = '#d183e8',
 		grey   = '#303030',
+		orange = '#f9e2af',
 	}
-
 	local bubbles_theme = {
 		normal = {
 			a = { fg = colors.black, bg = colors.violet },
 			b = { fg = colors.white, bg = colors.grey },
-			c = { fg = colors.black, bg = colors.black },
+			c = { fg = colors.white, bg = "none" },
 		},
 
-		insert = { a = { fg = colors.black, bg = colors.blue } },
-		visual = { a = { fg = colors.black, bg = colors.cyan } },
-		replace = { a = { fg = colors.black, bg = colors.red } },
+		insert = {
+			a = { fg = colors.black, bg = colors.blue },
+			c = { fg = colors.white }
+		},
+		visual = { a = { fg = colors.black, bg = colors.cyan }, c = { fg = colors.white } },
+		replace = { a = { fg = colors.black, bg = colors.red }, c = { fg = colors.white } },
 
 		inactive = {
-
-			a = { fg = colors.white, bg = colors.black },
-			b = { fg = colors.white, bg = colors.black },
-			c = { fg = colors.black, bg = colors.black },
+			a = { fg = colors.white, bg = colors.grey },
+			b = { fg = colors.white, bg = colors.grey },
+			c = { fg = colors.black, bg = colors.grey },
+			--
+			-- 	-- a = { fg = colors.white, bg = colors.black },
+			-- 	-- b = { fg = colors.white, bg = colors.black },
+			-- 	-- c = { fg = colors.black, bg = colors.black },
 		},
 	}
+
+	local function modified()
+		if vim.bo.modified then
+			return '+'
+		elseif vim.bo.modifiable == false or vim.bo.readonly == true then
+			return '-'
+		end
+		return ''
+	end
 
 	lualine.setup({
 		options = {
 			theme = bubbles_theme,
-			component_separators = '|',
+			component_separators = ' ',
 			section_separators = { left = '', right = '' },
 		},
 		sections = {
 			lualine_a = {
 				{ 'mode', separator = { left = '' }, right_padding = 2 },
 			},
-			lualine_b = { 'filename', 'branch' },
-			lualine_c = { 'fileformat' },
+			lualine_b = {
+				-- 'filename', 'branch'
+				'branch',
+				'diff',
+				{
+					'%r',
+					cond = function()
+						return vim.bo.readonly
+					end,
+				},
+				{ modified, color = { bg = colors.blue }, separator = { right = "" } },
+			},
+			lualine_c = { 'fileformat',
+				{ 'filename', file_status = false, path = 1 },
+			},
 			lualine_x = {},
-			lualine_y = { 'filetype', 'progress' },
+			lualine_y = {
+				{
+					'diagnostics',
+					source = { 'nvim' },
+					sections = { 'error' },
+					diagnostics_color = { error = { bg = colors.red, fg = colors.white } },
+				},
+				{
+					'diagnostics',
+					source = { 'nvim' },
+					sections = { 'warn' },
+					diagnostics_color = { warn = { bg = colors.orange, fg = colors.black } },
+				},
+				"filetype", "progress" },
 			lualine_z = {
 				{ 'location', separator = { right = '' }, left_padding = 2 },
 			},
