366c55f157e743c943191302db85bf1b248e6cc6
pn00@dearch <prateek.n2000@ymail.com>
2023-11-26 23:19:46 +0530

    Corrected rust-analyzer false errors

--- lua/dearch/lsp/settings/rust_analyzer.lua
+++ lua/dearch/lsp/settings/rust_analyzer.lua
@@ -1,12 +1,26 @@
 return {
-  require'lspconfig'.rust_analyzer.setup{
-	cmd = { "rustup", "run", "stable", "rust-analyzer" },
-	settings = {
-	  ['rust-analyzer'] = {
-		diagnostics = {
-		  enable = true;
+	require 'lspconfig'.rust_analyzer.setup {
+		cmd = { "rustup", "run", "stable", "rust-analyzer" },
+		settings = {
+			['rust-analyzer'] = {
+				diagnostics = {
+					enable = true,
+				},
+				imports = {
+					granularity = {
+						group = "module",
+					},
+					prefix = "self",
+				},
+				cargo = {
+					buildScripts = {
+						enable = true,
+					},
+				},
+				procMacro = {
+					enable = true
+				},
+			}
 		}
-	  }
 	}
-  }
 }
